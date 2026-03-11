44f912e17d01df6a677df6cabae9b36a3339d970
manoj-pun <manojpun840@gmail.com>
2026-03-11 17:27:54 +0545

    case conversion and searching methods

---
+++ 2_StringandStringMethods/StringMethods.py
@@ -0,0 +1,58 @@
+
+##########CASE CONVERSION METHODS###########
+#upper()
+# text = "hello world"
+# result = text.upper() #changes each letter to upper
+# print(result)
+
+#lower()
+# text = "HellO WorLD"
+# result = text.lower() #changes each letter to lower
+# print(result)
+
+#title()
+# text = "hello world"
+# result = text.title() #capitalizes each word
+# print(result)
+
+#capitalize()
+# text = "hello world"
+# result = text.capitalize() #capitalizes first letter
+# print(result)
+
+#swapcase()
+# text = "Hello World"
+# result = text.swapcase() #swaps lower to upper and upper to lower
+# print(result)
+
+
+
+
+#########Searching Methods############
+
+#find()
+# text = "python programming"
+# result = text.find("pro") #Returns index (or -1 if not found)
+# print(result)
+
+#index()
+# text = "python programming"
+# result = text.index("z") #Returns index (error if not found)
+# print(result)
+
+#count()
+# text = "python programming"
+# result = text.count("m") #Counts occurrences
+# print(result)
+
+# startswith()
+# text = "python programming"
+# result = text.startswith("py") #checks beginning and shows result in boolean value
+# print(result)
+
+#endswith()
+# text = "python programming"
+# result = text.endswith("on") #checks ending and shows result in boolean value
+# print(result)
+
+
