e2e5445d10b82735837892e17932837d21c1ea9b
manoj-pun <manojpun840@gmail.com>
2026-03-13 19:59:43 +0545

    updated question4

--- 3_ListandTuples/question4.py
+++ 3_ListandTuples/question4.py
@@ -3,33 +3,64 @@
 # point tuples and returns the one closest to (0, 0, 0). Use tuple unpacking.
 
 #Euclidean Distance Function
-import math
+# import math
 
-def distance(p1, p2):
-    x1, y1, z1 = p1
-    x2, y2, z2 = p2
+# def distance(p1, p2):
+#     x1, y1, z1 = p1
+#     x2, y2, z2 = p2
     
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+#     return math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
 
-p1 = (1,2,3)
-p2 = (4,5,6)
+# p1 = (1,2,3)
+# p2 = (4,5,6)
 
-print(distance(p1, p2))
+# print(distance(p1, p2))
 
 
-# Closest Point to Origin
-def closest_to_origin(points):
-    origin = (0,0,0)
+# # Closest Point to Origin
+# def closest_to_origin(points):
+#     origin = (0,0,0)
     
-    closest = points[0]
-    min_dist = distance(points[0], origin)
+#     closest = points[0]
+#     min_dist = distance(points[0], origin)
     
-    for point in points:
-        if distance(point, origin) < min_dist:
-            min_dist = distance(point, origin)
-            closest = point
+#     for point in points:
+#         if distance(point, origin) < min_dist:
+#             min_dist = distance(point, origin)
+#             closest = point
             
-    return closest
+#     return closest
+
+# points = [
+#     (3,4,5),
+#     (1,1,1),
+#     (2,2,2),
+#     (10,10,10)
+# ]
+
+# print(closest_to_origin(points))
+
+
+
+#more pythonic
+# import math
+
+# def closest_to_origin(points):
+#     return min(points, key=lambda p: math.sqrt(p[0]**2 + p[1]**2 + p[2]**2))
+
+# points = [
+#     (3,4,5),
+#     (1,1,1),
+#     (2,2,2),
+#     (10,10,10)
+# ]
+
+# print(closest_to_origin(points))
+
+
+#Even Cleaner Version (No sqrt Needed)
+def closest_to_origin(points):
+    return min(points, key=lambda p: p[0]**2 + p[1]**2 + p[2]**2)
 
 points = [
     (3,4,5),
