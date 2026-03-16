6d6349530b7337eae70c6c8dcde537798578f16c
manoj-pun <manojpun840@gmail.com>
2026-03-16 07:28:32 +0545

    question1

---
+++ 6_loops_for_while_comprehensions/question1.py
@@ -0,0 +1,28 @@
+# Using a for loop, print a right-angled triangle pattern of stars where each row has one more star
+# than the previous. For n=5 the output should be: row 1 has 1 star, row 2 has 2 stars... row 5 has
+# 5 stars. Then print the same triangle using a while loop. Then do it with a single list
+# comprehension.
+
+
+#for loop
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+
+#while loop
+# n = 5
+# i = 1
+
+# while i <= n:
+#     j = 1
+#     while j <= i:
+#         print("*", end=" ")
+#         j += 1
+#     print()
+#     i += 1
+
+#comprehension
+n = 5
+[print("* " * i) for i in range(1, n + 1)]
