236897eacd391d79b8c96c84bf2f659a6243d195
manoj-pun <manojpun840@gmail.com>
2026-03-18 13:17:19 +0545

    refactor question1

--- Basics/If_Else/question1.py
+++ Basics/If_Else/question1.py
@@ -4,13 +4,13 @@
 
 score = int(input("Enter a score: "))
 
-if score >= 90 and score <= 100:
+if 90 <= score <= 100:
     print("A")
-elif score >= 80 and score <= 89:
+elif 80 <= score <= 89:
     print("B")
-elif score >= 70 and score <= 79:
+elif 70 <= score <= 79:
     print("C")
-elif score >= 60 and score <= 69:
+elif 60 <= score <= 69:
     print("D")
 else:
     print("F")
