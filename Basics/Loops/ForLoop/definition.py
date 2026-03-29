451d4b6b95980cddd0fb74a3ed95cfa748909364
manoj-pun <manojpun840@gmail.com>
2026-03-29 18:34:45 +0545

    changes

--- Basics/Loops/ForLoop/definition.py
+++
@@ -1,9 +0,0 @@
-# A for loop is used to iterate over a sequence (like a list, string, tuple, dictionary keys, or range of numbers) 
-# a fixed number of times.
-# It is generally used when the number of iterations is known beforehand.
-
-for i in range(5):
-    print(i)
-
-# Here, the number of iterations is known beforehand → 5 times
-# This is why for is used when the number of repeats is fixed
--- Basics/Loops/ForLoop/question11.py
+++
@@ -1,5 +0,0 @@
-# Write a for loop using range() in REVERSE to print numbers from 10 down to 1, then print 'Liftoff!'
-
-for i in range(10,0,-1):
-    print(i)
-print("Liftoff!")
