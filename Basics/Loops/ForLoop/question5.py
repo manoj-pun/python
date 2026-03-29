f62318bc2417fd91e3b5fefb5c489252ae1e6dbe
manoj-pun <manojpun840@gmail.com>
2026-03-29 18:33:41 +0545

    changes

--- Basics/Loops/ForLoop/question5.py
+++
@@ -1,7 +0,0 @@
-# Use a for loop with enumerate() to print both the INDEX and VALUE of each element.
-# fruits = ["mango", "kiwi", "grape", "pear"]
-
-fruits = ["mango", "kiwi", "grape", "pear"]
-
-for index, value in enumerate(fruits):
-    print(f"{index}:{value}")
