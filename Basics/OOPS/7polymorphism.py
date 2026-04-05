d0f7cd858aaa88d9b4bee542b1687e34c6d1a525
manoj-pun <manojpun840@gmail.com>
2026-04-05 07:32:07 +0545

    polymorphism

--- Basics/OOPS/7polymorphism.py
+++ Basics/OOPS/7polymorphism.py
@@ -6,19 +6,45 @@
 #         1.Inheritance
 #         2.Duck Typing
 
+from abc import ABC,abstractmethod
+
 class Shape:
-    pass
+
+    @abstractmethod
+    def area(self):
+        pass
 
 class Circle(Shape):
-    pass
+
+    def __init__(self,radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * self.radius ** 2
 
 class Square(Shape):
-    pass
+
+    def __init__(self,side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
 
 class Triangle(Shape):
-    pass
 
-shapes = [Circle(),Square(),Triangle()]
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+    
+class Pizza(Circle):
+    def __init__(self,topping,radius):
+        super().__init__(radius)
+        self.topping = topping
+
+shapes = [Circle(4),Square(5),Triangle(4,5),Pizza("pepperoni",15)]
 
 for shape in shapes:
-    print(shape)
+    print(shape.area())
